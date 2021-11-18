59c5fed96cc3f41067d3c11a9091eeab41233e73
james tuyuc <jaimetuyuc7@gmail.com>
2021-11-18 09:50:09 -0600

    Base Version Native app
    Base version native side app for restaurant

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (  
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Hola restaurante app</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+ 
+export default App;
